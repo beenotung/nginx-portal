10cdd55165f266ad5d6bdb70d9d12455afb889e0
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-10-01 04:04:46 +0800

    feat: finish interactive mode to run the whole flow

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { execFileSync } from 'child_process'
 import {
   copyFileSync,
   existsSync,
@@ -6,7 +7,7 @@ import {
   readdirSync,
   writeFileSync,
 } from 'fs'
-import { basename, join } from 'path'
+import { basename } from 'path'
 import { createInterface } from 'readline'
 
 export type Config = {
@@ -124,12 +125,17 @@ export function scan_conf_dir(dir: string): Config[] {
   return config_list
 }
 
-export function parse_conf_file(file: string): Config {
-  let text = load_file(file).trim()
+export function parse_conf_lines(text: string): string[] {
   let lines = text
     .split('\n')
     .map(line => line.split('#')[0].trim())
     .filter(line => line.length > 0)
+  return lines
+}
+
+export function parse_conf_file(file: string): Config {
+  let text = load_file(file).trim()
+  let lines = parse_conf_lines(text)
   let server_name = lines
     .find(line => line.startsWith('server_name '))
     ?.replace('server_name ', '')
@@ -174,6 +180,61 @@ server {
   save_file(file, text)
 }
 
+export function update_conf_file(file: string) {
+  const text = load_file(file)
+
+  let new_text = text.replaceAll('\r', '')
+
+  for (;;) {
+    let t = new_text.replaceAll('\n\n\n\n', '\n\n\n')
+    if (t == new_text) {
+      break
+    }
+    new_text = t
+  }
+
+  let lines = parse_conf_lines(new_text)
+
+  // look for line: "listen 443 ssl http2; # managed by Certbot"
+  //       or line: "listen 443 http2 ssl; # managed by Certbot"
+  let has_http2 = lines.some(line => {
+    if (!line.startsWith('listen 443 ')) {
+      return false
+    }
+    let parts = line.split(';')[0].split(' ')
+    return parts.includes('ssl') && parts.includes('http2')
+  })
+
+  // look for lines: "listen 443 ssl; # managed by Certbot"
+  let has_ssl = lines.some(line => line.startsWith('listen 443 ssl'))
+
+  if (!has_http2 && has_ssl) {
+    let lines = new_text.split('\n')
+    for (let i = 0; i < lines.length; i++) {
+      let line = lines[i]
+      if (line.trimStart()[0] == '#') {
+        continue
+      }
+      lines[i] = line.replace('listen 443 ssl;', 'listen 443 ssl http2;')
+    }
+    new_text = lines.join('\n')
+  }
+
+  if (new_text != text) {
+    save_file(file, new_text)
+  }
+
+  if (!has_http2 && !has_ssl) {
+    // not obtained ssl cert by certbot yet
+    return 'no ssl' as const
+  }
+}
+
+function join(...parts: string[]) {
+  // use linux convention even when the script is generated on windows (to be run on linux server)
+  return parts.join('/')
+}
+
 function save_file(file: string, text: string) {
   console.log('save file:', file)
   writeFileSync(file, text.trim() + '\n')
@@ -196,6 +257,8 @@ let config_list_file = 'nginx.md'
 
 let config_dir = '/etc/nginx/conf.d'
 let draft_dir = 'draft/conf.d'
+
+let bash_file = 'draft/update.sh'
 
 if (!__filename.endsWith('.js')) {
   config_dir = 'mock/conf.d'
@@ -226,22 +289,123 @@ export let modes = {
     let config_list = scan_conf_dir(config_dir)
     let text = format_config_list(config_list)
     save_file(config_list_file, text)
+    console.log()
+    console.log(
+      '[message] please update file:',
+      config_list_file,
+      'to continue',
+    )
   },
   apply_config() {
     let text = load_file(config_list_file)
     let config_list = parse_config_list(text)
+
+    let no_ssl = false
+    let lines: string[] = []
+
     mkdirSync(draft_dir, { recursive: true })
+
     for (let config of config_list) {
       let src = join(config_dir, config.filename)
       let dest = join(draft_dir, config.filename)
-      if (existsSync(src)) {
-        copyFileSync(src, dest)
-      } else {
+      if (!existsSync(src)) {
         save_conf_file({ dir: draft_dir, config })
+        continue
       }
-      a
+
+      copyFileSync(src, dest)
+      let update_result = update_conf_file(dest)
+
+      if (update_result == 'no ssl') {
+        no_ssl = true
+        continue
+      }
+
+      if (is_file_same(src, dest)) {
+        // already updated
+        continue
+      }
+
+      lines.push(`sudo cp ${JSON.stringify(dest)} ${JSON.stringify(src)}`)
+    }
+
+    if (no_ssl || lines.length > 0) {
+      lines.push(`sudo nginx -t`)
+      lines.push(`sudo service nginx restart`)
+    }
+
+    if (no_ssl) {
+      lines.push(`sudo certbot --nginx`)
+      lines.push(
+        `echo "Hint: run nginx-portal again to enable http2 in nginx configs"`,
+      )
+    }
+
+    save_file(bash_file, lines.join('\n'))
+
+    console.log()
+    console.log('[message] please run file:', bash_file, 'to continue')
+  },
+  show_bash() {
+    let text = load_file(bash_file)
+    console.log()
+    console.log(`content of ${bash_file}:`)
+    console.log('```')
+    console.log(text.trim())
+    console.log('```')
+  },
+  run_bash() {
+    let out = execFileSync('bash', [bash_file]).toString()
+    console.log(out.trimEnd())
+  },
+  async interactive() {
+    for (;;) {
+      console.log(
+        `
+Select an action:
+0. exit
+1. scan nginx configs
+2. apply nginx configs
+3. show ${bash_file}
+4. run ${bash_file}
+`.trim(),
+      )
+      let ans = await ask('action: ')
+      ans = ans.toLowerCase()
+      switch (ans) {
+        case '0':
+        case 'exit':
+        case '.exit':
+          return
+        case '1':
+        case 'scan':
+          modes.scan_config()
+          break
+        case '2':
+        case 'apply':
+          modes.apply_config()
+          break
+        case '3':
+        case 'show':
+          modes.show_bash()
+          break
+        case '4':
+        case 'run':
+          modes.run_bash()
+          break
+        default:
+          console.error('Error: unknown action')
+          break
+      }
+      console.log()
     }
   },
+}
+
+function is_file_same(a_file: string, b_file: string) {
+  let a_text = readFileSync(a_file).toString().trim()
+  let b_text = readFileSync(b_file).toString().trim()
+  return a_text == b_text
 }
 
 function showHelp() {
@@ -308,36 +472,8 @@ async function cli() {
     process.exit(1)
   }
   if (interactive_flag) {
-    // TODO
-    for (;;) {
-      console.log(
-        `
-Select an action:
-0. exit
-1. scan nginx configs
-2. apply nginx configs
-`.trim(),
-      )
-      let ans = await ask('action: ')
-      ans = ans.toLowerCase()
-      switch (ans) {
-        case '0':
-        case 'exit':
-        case '.exit':
-          return
-        case '1':
-        case 'scan':
-          modes.scan_config()
-          break
-        case '2':
-        case 'apply':
-          modes.apply_config()
-          break
-        default:
-          console.error('Error: unknown action')
-          break
-      }
-    }
+    await modes.interactive()
+    return
   }
   if (scan_config_flag) {
     modes.scan_config()
